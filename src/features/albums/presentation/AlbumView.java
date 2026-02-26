cadac2458f56908e991e566bb54f895a715ee4ce
cgjmac <carlagj04@hotmail.com>
2026-02-26 12:18:51 +0100

    instancia de memoria en la feature de songs y authors

--- src/features/albums/presentation/AlbumView.java
+++ src/features/albums/presentation/AlbumView.java
@@ -1,6 +1,7 @@
 package features.albums.presentation;
 
 import features.albums.data.AlbumDataRepository;
+import features.albums.data.AlbumMemLocalDataSource;
 import features.albums.domain.Album;
 import features.albums.domain.GetAlbumsUseCase;
 import features.albums.domain.SaveAlbumUseCase;
@@ -10,8 +11,7 @@ import java.util.ArrayList;
 public class AlbumView {
     public static void printAlbums(){
         GetAlbumsUseCase getAlbumsUseCase = new GetAlbumsUseCase(
-                new AlbumDataRepository(AlbumMemLocalDataSource.newInstance())
-        );
+                new AlbumDataRepository(AlbumMemLocalDataSource.newInstance()));
         ArrayList<Album> albums = getAlbumsUseCase.execute();
         System.out.println(albums);
     }
--- src/features/songs/presentation/SongView.java
+++ src/features/songs/presentation/SongView.java
@@ -1,14 +1,33 @@
 package features.songs.presentation;
 
 
+import features.songs.data.SongDataRepository;
+import features.songs.data.SongMemLocalDataSource;
+import features.songs.domain.GetSongsUseCase;
+import features.songs.domain.Song;
+
+import java.util.ArrayList;
+
 public class SongView {
     public static void printSongs(){
-        GetSongsUseCase getSongsUseCase = new GetSongsUseCase(new SongDataRepository());
-        ArrayList<Song> songsList = getSongsUseCase.execute();
-
-
-        System.out.println(songsList);
-
-
+        GetSongsUseCase getSongsUseCase = new GetSongsUseCase(
+                new SongDataRepository(SongMemLocalDataSource.newInstance())
+        );
+        ArrayList<Song> songs = getSongsUseCase.execute();
+        System.out.println(songs);
     }
+
+    public static void saveAlbum(){
+
+        Song newSong = new Song();
+        SaveSongUseCase saveSongUseCase = new SaveSongUseCase(
+                new SongDataRepository(SongMemLocalDataSource.newInstance())
+        );
+        saveSongUseCase.execute(newSong);
+
+
+        printSongs();
+    }
+
+}
 }
